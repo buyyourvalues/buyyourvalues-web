ba1dc69a05224c98443f46f7608e931f4175f5dc
Erik Hinton <erik@erik-m5500i-R3.(none)>
2010-11-07 11:57:29 -0500

    Fixed styles

--- yelp.rb
+++ yelp.rb
@@ -31,7 +31,7 @@ class Yelp
   end
 end
 
-Business.all(:id => (91..191)).each do |b|
+Business.all(:id => (192..285)).each do |b|
   y = Yelp.new(b.name)
   if not y.businesses.empty?
     puts "Adding lat/long to #{b.name}"
