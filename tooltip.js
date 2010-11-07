4baeaaa8b04cac3ce23cd2c7ffccb25850ea58f7
Erik Hinton <erik@erik-m5500i-R3.(none)>
2010-11-07 05:49:55 -0500

    Added tooltip style stuff

--- tooltip.js
+++ tooltip.js
@@ -1,13 +1,3 @@
 BYV.template = _.template($('#google_map_tooltip_tmp').html(););
 $('#byv_tooltip').html(BYV.template());
 
-<script type="text/jst" id="google_map_tooltip_tmp">
-<% BYV.tooltip_iter = 0; %>
-<h2 id="tooltip_title"><%= BYV.current_biz.business_name  %></h2>
-  <ul id="tooltip_contributions">
-    <% _.each(BYV.current_biz.contributions,function(c){  %>
-      <li class="tooltip_item <%= BYV.tooltip_iter % 2 === 0 ? 'even' : 'odd'  %>"><%= c.amount  %> | <%= c.party  %></li>
-      <% tooltip_iter ++; %>
-    <% }); %>
-  <ul>
-</script>
