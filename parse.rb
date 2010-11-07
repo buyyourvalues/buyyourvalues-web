ad6a32bd4c0606df9c281cdbc7c19b8f04ad4a5c
Michael Louis Thaler <michael.louis.thaler@gmail.com>
2010-11-07 01:16:00 -0400

    Business categories working

--- parse.rb
+++ parse.rb
@@ -3,14 +3,28 @@ require 'rubygems'
 require 'fastercsv'
 require 'model.rb'
 
-FasterCSV.foreach("contributions.csv",{:headers => true}) do |row|
+FasterCSV.foreach("catcodes.csv", :headers => true) do |row|
+  BusinessCategory.create(
+    :id => row['code'],
+    :source => row['source'],
+    :name => row['name'],
+    :industry => row['industry'],
+    :order => row['order']
+  )
+end
+
+FasterCSV.foreach("contributions.csv", :headers => true) do |row|
   business = Business.first_or_create(
-    :name => row["organization_name"] || row["contributor_employer"]
+    :name => row["organization_name"] || row["contributor_employer"],
+    :business_category_id => row["contributor_category"] || ''
   )
 
   contributor = Contributor.first_or_create(
     :name => row["contributor_name"],
     :type => row["contributor_type"],
+    :address => row["contributor_address"],
+    :city => row["contributor_city"],
+    :state => row["contributor_state"],
     :zipcode => row["contributor_zipcode"],
     :occupation => row["contributor_occupation"] || '',
     :business => business
