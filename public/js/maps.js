d1797b4b8b3be13b815db6134500552a8fd3ff5a
Juan C. Muller <jcmuller@gmail.com>
2010-11-07 02:20:15 -0500

    Work towards live data loading.

--- public/js/maps.js
+++ public/js/maps.js
@@ -14,46 +14,55 @@ var initialize_map = function ()
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
-    google.maps.event.addListenerOnce(map, 'tilesloaded', addMarkers);
+    google.maps.event.addListenerOnce(map, 'tilesloaded', getBusinesses());
 }
 
 var addMarkers = function ()
 {
     var bounds = map.getBounds();
+
     var southWest = bounds.getSouthWest();
     var northEast = bounds.getNorthEast();
-    var lngSpan = northEast.lng() - southWest.lng();
-    var latSpan = northEast.lat() - southWest.lat();
-    for (var i = 0; i < 10; i++) {
+    var lngSpan   = northEast.lng() - southWest.lng();
+    var latSpan   = northEast.lat() - southWest.lat();
+
+    // Create random markers
+    for (var i = 0; i < 100; i++)
+    {
         var latLng = new google.maps.LatLng(southWest.lat() + latSpan * Math.random(),
                 southWest.lng() + lngSpan * Math.random());
-        var marker = new google.maps.Circle({
-            center: latLng,
-            map: map,
-            radius: 50,
-            strokeOpacity: 0,
-            fillColor: "#cc0000",
-            fillOpacity: 0.7
-        });
+        addMarker(latLng);
     }
+}
 
-    // Add a Circle overlay to the map
-    var gop_circle = new google.maps.Circle({
-        center: map.getCenter(),
+var addMarker = function (latLng)
+{
+    var r = Math.floor(Math.random() * 2);
+
+    new google.maps.Circle({
+        center: latLng,
         map: map,
         radius: 50,
         strokeOpacity: 0,
-        fillColor: "#cc0000",
-        fillOpacity: 0.7
+        fillColor: "#0000ff",
+        fillOpacity: 1.0
     });
+}
 
-    var dem_circle = new google.maps.Circle({
-        center: new google.maps.LatLng(37.4440, -122.1440),
-        map: map,
-        radius: 50,
-        strokeOpacity: 0,
-        fillColor: "#0000cc",
-        fillOpacity: 0.7
-    });
+var getBusinesses = function ()
+{
+    $.get(
+        '/businesses'
+        {},
+        function (resp) {
+            console.log(resp);
+        },
+        'json'
+    );
+}
+
+
+var processBusiness = function (business)
+{
 
 }
